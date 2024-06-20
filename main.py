0c63c5a0da12c8a36f2ed9491fdd3d735f6bd7eb
luzede <85195923+luzede@users.noreply.github.com>
2024-06-20 18:46:24 +0300

    fixed an issue where running the script from a different place, would create files somewhere else

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import requests
 import feedparser
 from markdownify import markdownify
 from discord_webhook import DiscordWebhook, DiscordEmbed
+from inspect import getsourcefile
+from os import path
 import time
 import datetime
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
+# Get the directory of this file
+current_dir = path.dirname(path.abspath(getsourcefile(lambda: 0)))
+
+config = dotenv_values(path.join(current_dir, ".env"))
 
 response = requests.get("https://georgians.gr/feed/")
 
 try:
-    with open("last_known_post_date.txt", "r") as f:
+    with open(path.join(current_dir, "last_known_post_date.txt"), "r") as f:
         last_known_post_date = time.strptime(f.read(), "%Y-%m-%dT%H:%M:%SZ")
 except FileNotFoundError:
     last_known_post_date = None
@@ -48,7 +53,7 @@ for entry in feed.entries:
         )
     )
 
-with open("last_known_post_date.txt", "w") as f:
+with open(path.join(current_dir, "last_known_post_date.txt"), "w") as f:
     f.write(time.strftime("%Y-%m-%dT%H:%M:%SZ", last_post_date_in_feed))
 
 webhook = DiscordWebhook(
